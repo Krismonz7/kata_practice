dc7ddcba5f7dced195b087d67a9f3341267f5366
Krismonz7 <kriz.monz707@gmail.com>
2024-01-02 12:34:12 -0500

    1 solution

--- exersise.py
+++ exersise.py
@@ -1,4 +1,19 @@
-#Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
-
-def bool_to_word(boolean):
-    return "Yes" if boolean else "No"
+#Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+#(In this case, all triangles must have surface greater than 0 to be accepted).
+import math
+def is_triangle(a, b, c):
+    def aviable(a,b,c):
+        a = a/2
+        b = b
+        height = a*a + b*b
+        height = height - b*b
+        height = math.sqrt(height)
+        area = (a * height) / 2
+        print(area)
+        if area > 0:
+            return True
+        
+        else:
+            return False
+    available = aviable(a,b,c)
+    return available
